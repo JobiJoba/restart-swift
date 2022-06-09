8f4a764e8f8ba2d7380fada03a9d32cf9063b958
JobiJoba <Johann.baziret@gmail.com>
2022-06-09 15:05:06 +0700

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by joba on 9/6/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
